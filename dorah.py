04f2ca9a3a3f58de19afc0cf2c5a1e9b341e7155
Connor Green <cgreen@launchdarkly.com>
2022-02-22 12:21:51 -0600

    pycache removed from tracking

--- dorah.py
+++ dorah.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 """
 STEP 1: The following two lines are imports within python. They manage the Python SDK referenced in the README and allow this app to connect to LD
-UNCOMMENT LINES 14 and 15 before proceeding.  You may need to look at the Pythond SDK Page in order find the correct import command
+UNCOMMENT lines 17 and 18 before proceeding.  You may need to look at the Pythond SDK Page in order find the correct import command
 """
 # import ldclient
 # from ldclient.config import Config
@@ -29,8 +29,8 @@ tips = sns.load_dataset('tips')
 # Set sdk_key to your LaunchDarkly SDK key before running
 """
 STEP 2: This is the key that will authenticate with your Launch Darkly Environment, lets the LaunchDarkly know where to look when the application reaches out to us.
-An SDK key is environment specific. In this case I am looking at the test environment of the cgreen environment in support service
-UNCOMMENT LINES 33 before proceeding
+An SDK key is environment specific. In this case I am looking at the `cgreen` environment in support service
+UNCOMMENT line 35 before proceeding
 """
 #sdk_key = "sdk-7084b95b-76c3-4353-95ed-fd75ba818bbc"
 
@@ -192,7 +192,7 @@ if __name__ == '__main__':
 
     """
     STEP 3: We are going to use the code from the import in step 1, in order to configure LaunchDarkly.
-    UNCOMMENT lines 194-201 to see the configuration for our environemnt passed.  We then check if our SDK was successfully initalized
+    UNCOMMENT lines 197-203 to see the configuration for our environemnt passed.  We then check if our SDK was successfully initalized
     """
     # ldclient.set_config(Config(sdk_key))
     # # The SDK starts up the first time ldclient.get() is called
@@ -204,13 +204,13 @@ if __name__ == '__main__':
 
     """
     STEP 4: Let's evaluate a flag, first we need an example
-    UNCOMMENT line 207 to reference a flag in this test environment
+    UNCOMMENT line 209 to reference a flag in this test environment
     """
     #feature_flag_key = "csm-dora-demo-test"
 
     """
-    STEP 5: We need to pass a user for sever side variatons, in this case we will just pass a key, the only required field
-    UNCOMMENT line 213 to create a user object
+    STEP 5: We need to pass a user for sever side variatons, in this case we will just pass a key, the only required field.  Other information can be added to this object: https://docs.launchdarkly.com/home/users/attributes
+    UNCOMMENT line 215 to create a user object
     """
     #user = {"key": "user@test.com"}
 
@@ -218,6 +218,7 @@ if __name__ == '__main__':
     STEP 6: Let's evaluate a flag!! We are going to do two things:
         - First pass the user, flag key, and our default evaluation (just in case something goes wrong) to the client we created
         - Then we are going to use an if statement to tell us if the flag matches what is currently in support service
+        UNCOMMENT lines 224-228
     """
     
     # show_feature = client.variation(feature_flag_key, user, False)
